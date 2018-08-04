576f2a11b95010459930607ff34c436406b5072a
AkashbabuNA <akash.babu@nestaway.com>
2018-08-04 23:04:50 +0530

    adds test for stag

--- test/env.test.js
+++ test/env.test.js
@@ -1,12 +1,13 @@
 import { expect } from 'chai';
 
-import { IsENVDev, IsENVProd, IsENVTest, GetENV } from '../src';
+import { IsENVDev, IsENVProd, IsENVTest, GetENV, IsENVStag } from '../src';
 
 describe('#env NODE_ENV test', () => {
     it('should return dev&prod as false if NODE_ENV=test', () => {
         expect(GetENV()).to.be.eql('test');
         expect(IsENVTest).to.be.true;
         expect(IsENVDev).to.be.false;
+        expect(IsENVStag).to.be.false;
         expect(IsENVProd).to.be.false;
     });
 });
