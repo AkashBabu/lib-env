550bf3f797aa99202d0d507b7055d7deb1c0b418
AkashbabuNA <akash.babu@nestaway.com>
2018-08-04 23:26:41 +0530

    Test changed from checking for false to undefined

--- test/env.test.js
+++ test/env.test.js
@@ -6,8 +6,8 @@ describe('#env NODE_ENV test', () => {
     it('should return dev&prod as false if NODE_ENV=test', () => {
         expect(GetENV()).to.be.eql('test');
         expect(IsENVTest).to.be.true;
-        expect(IsENVDev).to.be.false;
-        expect(IsENVStag).to.be.false;
-        expect(IsENVProd).to.be.false;
+        expect(IsENVDev).to.be.undefined;
+        expect(IsENVStag).to.be.undefined;
+        expect(IsENVProd).to.be.undefined;
     });
 });
